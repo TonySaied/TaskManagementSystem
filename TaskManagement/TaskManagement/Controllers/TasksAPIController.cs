f283119b95a06e0771f2937341860d7f2fd9c7ca
TonySaied <63235281+TonySaied@users.noreply.github.com>
2024-08-03 23:19:31 +0300

    Tasks API Controller

---
+++ TaskManagement/TaskManagement/Controllers/TasksAPIController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TaskManagement.Models;
+
+namespace TaskManagement.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TasksAPIController : ControllerBase
+    {
+        ApplicationDbContext _context;
+        public TasksAPIController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        //https://localhost:7124/api/TasksApi/Overdue?count=2 => Testing Link
+        [HttpGet("Overdue")]
+        public IActionResult GetOverdueTasks(int count)
+        {
+            var overdueTasks = _context.Tasks
+                .Where(t => t.DueDate < DateTime.Now).Take(count).ToList();
+
+            return Ok(overdueTasks);
+        }
+    }
+}
